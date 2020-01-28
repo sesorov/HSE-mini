b8615526537145f12acbc9808c27a63283cbdf93
Alexander Sesorov <samws@mail.ru>
2020-01-28 11:54:57 +0300

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,113 +2,52 @@
 #define print std::cout
 using std::endl;
 using std::string;
-const char SUM = 0;
-const char SUB = 1;
-const char MUL = 2;
-const char DIV = 3;
-
-void setOperation(char in, char &temp_OpType)
-{
-    switch (in)
-    {
-        case '+':
-        {
-            temp_OpType = SUM;
-            break;
-        }
-        case '-':
-        {
-            temp_OpType = SUB;
-            break;
-        }
-        case 'm':
-        {
-            temp_OpType = MUL;
-            break;
-        }
-        case '/':
-        {
-            temp_OpType = DIV;
-            break;
-        }
-        default:
-            break;
-    }
-}
 
 int main(int argc, const char * argv[])
 {
     using namespace std;
-    cout << argc << endl;
-    char operation = SUM, temp_operation = operation;
-    int res(0);
-    for (int i = 1; i < argc; ++i)
+    int result = atoi(argv[1]);
+    for (int i = 2; i < argc; i += 2)
     {
-        if (operation == SUM) // +
+        if (*argv[i] == '+') result += atoi(argv[i + 1]);
+        if (*argv[i] == '-') result -= atoi(argv[i + 1]);
+        if (*argv[i] == 'x')
         {
-            if (i % 2 != 0)
+            if (*argv[i - 2] == '-')
             {
-                res += atoi(argv[i]);
+                result += atoi(argv[i - 1]);
+                result -= (atoi(argv[i+1]) * atoi(argv[i-1]));
+            }
+            else if (*argv[i - 2] == '+')
+            {
+                result -= atoi(argv[i - 1]);
+                result += (atoi(argv[i+1]) * atoi(argv[i-1]));
+            }
+            else result *= atoi(argv[i + 1]);
+        }
+        if (*argv[i] == '/')
+        {
+            if (!atoi(argv[i+1]))
+            {
+                printf("Error: division by zero.\n");
+                exit(1);
             }
             else
             {
-                temp_operation = operation;
-                setOperation(*argv[i], operation);
-            }
-        }
-        else if (operation == SUB) // -
-        {
-            if (i % 2 != 0)
-            {
-                res -= atoi(argv[i]);
-            }
-            else
-            {
-                temp_operation = operation;
-                setOperation(*argv[i], operation);
-            }
-        }
-        else if (operation == MUL || operation == DIV)
-        {
-            if (i % 2 != 0)
-            {
-                switch (temp_operation)
+                if (*argv[i - 2] == '-')
                 {
-                    case SUM:
-                    {
-                        res -= atoi(argv[i - 2]);
-                        res += (operation == MUL) ? (atoi(argv[i - 2]) * atoi(argv[i])) : (atoi(argv[i - 2]) / atoi(argv[i]));
-                        break;
-                    }
-                    case SUB:
-                    {
-                        res += atoi(argv[i - 2]);
-                        res -= (operation == MUL) ? atoi(argv[i - 2]) * atoi(argv[i]) : atoi(argv[i - 2]) / atoi(argv[i]);
-                        break;
-                    }
-                    case MUL:
-                    {
-                        if (operation == MUL) res *= atoi(argv[i]);
-                        else res /= atoi(argv[i]);
-                        break;
-                    }
-                    case DIV:
-                    {
-                        if (operation == DIV) res /= atoi(argv[i]);
-                        else res *= atoi(argv[i]);
-                        break;
-                    }
-                    default:
-                        break;
+                    result += atoi(argv[i - 1]);
+                    result -= (atoi(argv[i-1]) / atoi(argv[i+1]));
                 }
-            }
-            else
-            {
-                temp_operation = operation;
-                setOperation(*argv[i], operation);
+                else if (*argv[i - 2] == '+')
+                {
+                    result -= atoi(argv[i - 1]);
+                    result += (atoi(argv[i-1]) / atoi(argv[i+1]));
+                }
+                else result /= atoi(argv[i + 1]);
             }
         }
     }
-    cout << "Res = " << res << endl;
+    cout << result << endl;
     return EXIT_SUCCESS;
 }
